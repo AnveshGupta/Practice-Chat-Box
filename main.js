4771bcc139c28611e0d676ffe10e1bfc093194b7
humangasaur <53492178+humangasaur@users.noreply.github.com>
2020-08-08 21:13:16 +0530

    Private Message added

--- main.js
+++ main.js
@@ -13,13 +13,25 @@ server =  app.listen(port,() => {
 });
 var io = require('socket.io')(server);
 
-io.on('connection',(socket) => {
-    console.log("connection established"); 
-    user = socket.handshake.query.user;
-    console.log(user);
-    console.log(socket.id);
-    users.push({user: username , socket: socket.id});
-    socket.on('chat message', (msg) => {
-        socket.broadcast.emit('chat message', msg);
+io.on('connect',(socket) => {
+    username = socket.handshake.query.user;
+    m_r_user = {user:username,socket:socket.client.id};
+    users.push(m_r_user);
+    // console.log(Object.keys(io.engine.clients)); // used to print the socketid of connected users.
+    // console.log(io.engine.clientsCount); 
+    io.emit('users',users);
+    socket.on('chat message', (data) => {
+
+        var user_send;
+        users.forEach(function(user){
+            if(user.socket == data.from){
+                user_from = user.user;
+                data_send = {from:user_from,msg:data.message}
+                io.to(data.to).emit('chat message', data_send);
+                // io.sockets.in(data.touser).emit('chat message', data_send );
+                return;
+            }
+        });
+
     });
 });
--- public/chat.js
+++ public/chat.js
@@ -1,23 +1,62 @@
-username =  null;
+users_online = [];
+this_user =  null;
+this_socket = null;
+setuser = null;
 $(document).ready(function(){
     username = prompt("Nickname : ");
     if(!username){
         username = "anoynomous";
     }
+    $("#currentusername").text("Welcome to chatbox : " + username);
 });
 
 $(function(){
     connectionprofile = "http://localhost:3000?user="+username;
-    console.log(connectionprofile);
     socket = io.connect(connectionprofile);
+    socket.on('connect', function() {
+        if(!this_socket){
+            this_socket = socket.id;
+        }
+    });
     $('form').submit(function(e) {
         e.preventDefault(); // prevents page reloading
         console.log($('#m').val());
-        socket.emit('chat message', $('#m').val());
+        socket.emit('chat message', {from: this_socket, to:setuser,message:$('#m').val()});
         $('#m').val('');
         return false;
     });
-    socket.on('chat message', function(msg){
-        $('#messages').append($('<li>').text(msg));
+    socket.on("users",function(users){
+        users.forEach(function(user){
+            found = false;
+            if(user.socket != this_socket){
+                users_online.forEach(function(list_user){
+                    if(list_user.socket == user.socket ){
+                        found = true;
+                    }
+                });
+                if(!found){
+                    users_online.push(user);
+                    var button = $("<button>");
+                    button.text(user.user);
+                    button.click(function () {
+                        touser(user);
+                    });
+                    // button.on("click", function () {
+                    //     touser(user);
+                    // });
+                    var li = $("<li>");
+                    li.append(button);
+                    $('#userlist').append(li);
+                }
+            }
+        });
+
+    });
+    socket.on('chat message', function(data){
+        $('#messages').append($('<li>').text(data.from + " : " +data.msg));
     });
 });
+function touser(user){
+    setuser = user.socket;
+    console.log(user.user);
+}
